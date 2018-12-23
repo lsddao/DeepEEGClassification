503857f6609d3d867c30aca21c05a18b12f9e7cf
lsddao <42513171+lsddao@users.noreply.github.com>
2018-12-23 13:03:44 +0100

    remove unused parameters

--- FFT2DDataProvider.py
+++ FFT2DDataProvider.py
@@ -8,24 +8,18 @@ class FFT2DDataProvider(WindowBasedDataProvider):
 		super().__init__(config, labelProvider)
 
 		self.timesteps = deque(maxlen=self.config.sequenceLength)
-		self.fft_increment = 2
-		self.fft_shift = 0
 
 	def X_shape(self):
 		return [-1, self.config.sequenceLength, self.config.nFeatures, 1]
 
 	def consumeWindow(self):
-		if len(self.timesteps) == self.timesteps.maxlen:
-			self.fft_shift += 1
 		features = []
 		f = []
 		for channel_idx in range(4):
-			f.append( fft_log(self.samples[channel_idx])[:64] )
+			f.append( fft_log(self.samples[channel_idx])[:self.config.nFeatures] )
 		features.extend(f[0] - f[3])
 		#features.extend(f[1] - f[2])
 		self.timesteps.append(features)
-		if self.fft_shift == self.fft_increment:
-			self.fft_shift = 0
 		if len(self.timesteps) == self.timesteps.maxlen:
 			if not self.isCurrentClassFull():
 				self.appendData(np.array(self.timesteps))
